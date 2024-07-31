eeddfa9b237058355b003ae14c8a3d8a3de5db2c
artin-paravar <149859777+artin-paravar@users.noreply.github.com>
2024-07-31 20:34:12 +0330

    a

--- frontend/src/app/filteritem.tsx
+++ frontend/src/app/filteritem.tsx
@@ -17,11 +17,11 @@ export const FilterApi = () => {
   const randomItem = productsQuery?.data?.items?.filter((item: Products) => {
     return [item][0]._id.includes("5");
   });
-  // const MahsolatMortabat = (category: any, id: any) => {
-  //   const data = productsQuery?.data?.items?.filter((item: any) => {
-  //     return item?.category?.includes(category) && item?._id != id;
-  //   });
-  //   return data;
-  // };
-  return { phoneCategory, consoleCategory, randomItem };
+  const MahsolatMortabat = (category: any, id: any) => {
+    const data = productsQuery?.data?.items?.filter((item: any) => {
+      return item?.category?.includes(category) && item?._id != id;
+    });
+    return data;
+  };
+  return { phoneCategory, consoleCategory, randomItem, MahsolatMortabat };
 };
