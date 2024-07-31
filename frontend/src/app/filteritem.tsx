8f847a914dd8b9483fad6538c76e30799f59a6c6
artin-paravar <149859777+artin-paravar@users.noreply.github.com>
2024-07-31 19:16:31 +0330

    a

--- frontend/src/app/filteritem.tsx
+++ frontend/src/app/filteritem.tsx
@@ -1,8 +1,10 @@
+import { useParams } from "react-router-dom";
 import { ProductsQuery } from "./services/queries";
 import { Products } from "./type/type";
 
 export const FilterApi = () => {
   const productsQuery = ProductsQuery();
+  const { id } = useParams<{ id: any }>();
 
   //men
   const phoneCategory = productsQuery?.data?.items?.filter((item: Products) => {
@@ -17,7 +19,7 @@ export const FilterApi = () => {
   const randomItem = productsQuery?.data?.items?.filter((item: Products) => {
     return [item][0]._id.includes("5");
   });
-  const MahsolatMortabat = (category: string, id: any) => {
+  const MahsolatMortabat = (category: string) => {
     const data = productsQuery?.data?.items?.filter((item: Products) => {
       return [item][0].category?.includes(category) && item?._id !== id;
     });
