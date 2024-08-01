a3000dfb5d0f83aaf8f5e2fa39d824b5bbc6c8e3
artin-paravar <149859777+artin-paravar@users.noreply.github.com>
2024-08-01 10:09:51 +0330

    font

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        iranssans: ["iransans"],
+      },
       colors: {
         whiteText: "#fff",
         darkText: "#000000",
